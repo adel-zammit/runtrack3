4dafa12c69e68ddaa3290f4d594ea83ff51e0825
SpectrePhantom <adel-zammit@users.noreply.github.com>
2020-09-18 13:28:57 +0200

    Jour06

---
+++ Jour06/Job01/index.php
@@ -0,0 +1,24 @@
+<DOCTYPE html>
+    <html>
+
+        <title>Jour06 - Job01</title>
+        <head>
+
+        <meta  charset = " utf-8 ">
+    <meta  name = " viewport "  content = " width = device-width, user-scalable = yes " />
+                <link rel="stylesheet" type="text/css" media="screen and (min-width:1680px) and (max-width:1920px)"  href="style1.css">
+                <link rel="stylesheet" type="text/css" media="screen and (min-width:1280px ) and (max-width:1680px)" href="style2.css">
+                <link rel="stylesheet" type="text/css" media="screen and (min-width:1920px)" href="style3.css">
+                <link rel="stylesheet" type="text/css" media="screen and (max-width:1280px)" href="style4.css" >
+
+            
+        </head>
+
+<body>
+
+
+<h1>Salut a tous<h1>
+
+</body>
+
+</html>
